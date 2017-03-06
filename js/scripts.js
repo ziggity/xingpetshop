e71ad630c8ada7ed81966f567a75d88cd8f3ff66
Eliot Carlsen and Xing Li <eliot+xing@>
2017-03-06 11:52:48 -0800

    add output fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,10 @@
-function Pet(name, age, color){
+function Pet(animalType, name, age, color){
+  this.animalType = animalType;
   this.name = name;
   this.age = age;
   this.color = color;
 };
 
-
-var inputPet = "";
-var petName = "";
-var petColor = "";
-var petAge = "";
-
-function inputs () {
-  inputPet = $("#dogAdoption2").val();
-  petName = $("#name").val();
-  petColor = $("#color").val();
-  petAge = parseInt($("#age").val());
-};
-
-function choose () {
-  if(inputPet==="cat"){
-    var cat = new Pet(petName,petAge,petColor);
-  } else if (inputPet === "dog") {
-    var dog = new Pet(petName,petAge,petColor);
-  } else if (inputPet === "pig") {
-    var pig = new Pet(petName,petAge,petColor);
-  }
-};
 
 
 
@@ -33,8 +12,32 @@ function choose () {
 $(document).ready(function(){
   $("#formInput1").submit(function(event){
     event.preventDefault();
-    inputs();
-    choose();
+    var petType = $("#dogAdoption2").val();
+    console.log(petType)
+    var petName = $("#name").val();
+    console.log(petName)
+    var petColor = $("#color").val();
+    console.log(petColor)
+    var petAge = parseInt($("#age").val());
+    console.log(petAge)
+
+  if(petType === "Cat"){
+    var cat = new Pet (petType, petName, petAge, petAge);
+    console.log(cat.name);
+    console.log("here")
+  } else if (petType==="Dog"){
+    var dog = new Pet (petType, petName, petAge, petAge);
+    console.log(dog);
+  } else if (petType==="Pig"){
+    var pig = new Pet (petType, petName, petAge, petAge);
+    console.log(pig);
+  };
+
+    $("#t1").append('<ul><li>Name: ' + cat.name + '</li><li>Age: ' + cat.age + '</li><li>Color: ' + cat.color + '</li>');
+    $("#t2").append('<ul><li>Name: ' + dog.name + '</li><li>Age: ' + dog.age + '</li><li>Color: ' + dog.color + '</li>');
+    $("#t3").append('<ul><li>Name: ' + pig.name + '</li><li>Age: ' + pig.age + '</li><li>Color: ' + pig.color + '</li>');
+
+
 
 
 
